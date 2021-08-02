1e5ee2235ce4556ee2b214fe04f0ef03493807c2
calvin <puram.calvin@gmail.com>
2021-08-02 16:46:06 +0100

    prevent cors

--- store/index.js
+++ store/index.js
@@ -40,7 +40,7 @@ export const actions = {
   async getWeather({ commit }, location) {
     try {
       const data = await this.$axios.$get(
-        `/api/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=ac42dded7173931a9479a45b91239723&units=metric`
+        `https://api.openweathermap.org/api/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=ac42dded7173931a9479a45b91239723&units=metric`
       )
       this.commit('SET_WEATHER', data)
     } catch (err) {}
